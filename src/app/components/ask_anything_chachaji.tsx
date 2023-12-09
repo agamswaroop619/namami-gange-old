c15ee1b247db4ff99a5efa33bef6c65696b55a47
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-09 20:13:04 +0530

    add: ask_anything_chachaji added

---
+++ src/app/components/ask_anything_chachaji.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+export default function ask_anything_chachaji() {
+  return (
+    <div
+      id="AskAnyhtingRoot"
+      className="relative flex flex-row justify-end w-full items-start pt-6 px-24"
+    >
+      <div className="text-center text-6xl font-['Luckiest_Guy'] text-black/78 absolute top-0 left-0 h-32 w-[510px]">
+        Ask anything to
+        <br />
+        Chacha Ji
+      </div>
+      <img
+        src="https://file.rendit.io/n/bHC5Aj2hrxG10Jr9i8Mi.png"
+        alt="ImageyucFFtITKftransformedremovebgpreview"
+        className="relative w-12 mt-8 mb-6"
+      />
+    </div>
+  );
+}
