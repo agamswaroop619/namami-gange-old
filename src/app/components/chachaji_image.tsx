f7293e4ad940e8a830f76bb15bc1a904e00a87fc
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-10 19:03:00 +0530

    add: chachaji_image

--- src/app/components/chachaji_image.tsx
+++ src/app/components/chachaji_image.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
-import chachaji from '../components/chacha-removebg-preview.jpg';
+import Image from "next/image";
+import chachaji from "../components/chacha-removebg-preview.jpg";
 
 export default function Chachajifull() {
-    return (
-      <div>
-        <Image src={chachaji} alt="vfda"/> 
-      </div>
-    );
-  }
-  
+  return (
+    <Image
+      src={chachaji}
+      alt="vfda"
+      className="min-w-full overflow-hidden mix-blend-multiply absolute bottom-0"
+    />
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
     <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE] relative">
       <Navbar />
       <section className="min-h-screen w-2/6 absolute inset-y-0 left-0">
-        <Chachajifull/>
+        <Chachajifull />
       </section>
-      <section className="min-h-screen w-4/6 absolute inset-y-0 right-0 bg-black">
+      <section className="min-h-screen w-4/6 absolute inset-y-0 right-0">
         <Ask />
       </section>
     </main>
