504e872bc83854828e8bbf1cace4400b549882ff
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-10 19:18:51 +0530

    add: chat tab started

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Navbar from "../components/navbar";
+import Textinput from "../components/textinput";
 export default function Home() {
   return (
     <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE]">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE] relative">
       <Navbar />
-      <section className="min-h-screen w-2/6 absolute inset-y-0 left-0">
+      <section className="min-h-screen w-2/6 absolute inset-y-0 left-0 select-none">
         <Chachajifull />
       </section>
-      <section className="min-h-screen w-4/6 absolute inset-y-0 right-0">
+      <section className="min-h-screen w-4/6 absolute inset-y-0 right-0 select-none">
         <Ask />
       </section>
     </main>
