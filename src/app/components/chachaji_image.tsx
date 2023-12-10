4fe3fe0896b88483da003ee61d99ccdef7c3a52f
Vanshika-stack <vanshikarajpal43@gmail.com>
2023-12-10 15:21:29 +0530

    add:vanshika commit 1/2

---
+++ src/app/components/chachaji_image.tsx
@@ -0,0 +1,8 @@
+export default function Chachajifull() {
+    return (
+      <div>
+         <img src="chacha-removebg-preview" alt="logo1" width="300px"/>
+      </div>
+    );
+  }
+  
--- src/app/components/icon.tsx
+++ src/app/components/icon.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+// import logo from './logo.png';
 export default function Navbar() {
-  return (
-    <div className="w-[150px] h-[150px] relative bg-stone-100 rounded-full shadow" />
-  );
+  // return (
+  //   <div className="w-[150px] h-[150px] relative bg-stone-100 rounded-full shadow" />
+  // );
+  <img src="logos.svg" alt="logo" width="300px"/>
 }
