c2ed7c1546209a126f56c07f564ffd86ccd4269c
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-10 07:38:54 +0530

    add: chat tab

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Navbar from "../components/navbar";
+export default function Home() {
+  return (
+    <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE]">
+      <Navbar />
+    </main>
+  );
+}
