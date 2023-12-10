aef689eaf9ebfd333340aa0df41887facb3dfe1f
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-10 14:30:49 +0530

    fix: previous commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE]">
       <Navbar />
-      <Ask />
+      <section>
+        <Ask />
+      </section>
+      <section></section>
     </main>
   );
 }
