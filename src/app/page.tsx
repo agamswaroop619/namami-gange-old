a3d74e70000124d17b2f2ab6a68984fc75c309db
Vanshika-stack <vanshikarajpal43@gmail.com>
2023-12-10 15:47:35 +0530

    add: chachaji on front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
 import Ask from "./components/ask_anything_chachaji";
+import Chachajifull from "./components/chachaji_image";
 export default function Home() {
   return (
     <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE] relative">
       <Navbar />
-      <section className="min-h-screen w-2/6 absolute inset-y-0 left-0"></section>
+      <section className="min-h-screen w-2/6 absolute inset-y-0 left-0">
+        <Chachajifull/>
+      </section>
       <section className="min-h-screen w-4/6 absolute inset-y-0 right-0 bg-black">
         <Ask />
       </section>
