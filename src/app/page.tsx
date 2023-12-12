6d51dff83235f12b79d0f967568553eb8b7641d8
aarat0802 <aaratprasad2002@gmail.com>
2023-12-12 12:15:41 +0530

    ADd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Ask from "./components/ask_anything_chachaji";
 import Chachajifull from "./components/chachaji_image";
 export default function Home() {
   return (
-    <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE] relative">
+    <main className="flex mi1n-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE] relative">
       <Navbar />
       <section className="min-h-screen w-2/6 absolute inset-y-0 left-0 select-none">
         <Chachajifull />
