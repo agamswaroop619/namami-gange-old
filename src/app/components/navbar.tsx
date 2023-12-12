02b7f1df3337a20cf1b640d4d8cc3558e1f8d5c2
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-12 21:22:12 +0530

    add: Navbar fixes

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import logowritten from "../components/output_image.jpeg";
 export default function Navbar() {
   return (
     <div
@@ -6,17 +8,13 @@ export default function Navbar() {
       className="shadow-[0px_4px_4px_0px_rgba(0,_0,_0,_0.25)] flex flex-row w-full h-[7vh] items-start z-10"
     >
       <div className="bg-[linear-gradient(180deg,_#ffffff_0%,#ffffff_93%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-between w-full items-start px-1">
-        <div className="flex flex-row mt-px gap-4 w-1/3 items-start">
+        <div className="flex flex-row mt-px gap-4 w-1/2 items-start">
           <img
             src="https://file.rendit.io/n/JjGgghQKDnMUWmpzEZNO.png"
             alt="Vector"
             className="w-16"
           />
-          <img
-            src="https://file.rendit.io/n/hX7FxvoMl3lZr8c3TSVa.svg"
-            alt="ClipPathGroup"
-            className="h-[7vh]"
-          />
+          <Image src={logowritten} alt="" className="h-16 w-80" />
         </div>
         <img
           src="https://file.rendit.io/n/9k7msNM2ENT1jEOy9aU9.png"
