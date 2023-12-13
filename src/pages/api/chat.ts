fee6553ad7b1043820b21941c14f58d912f8187e
kjurel <89933773+kjurel@users.noreply.github.com>
2023-12-13 21:35:29 +0530

    add: chat route

---
+++ src/pages/api/chat.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { OpenAIClient, AzureKeyCredential } from "@azure/openai";
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  const { method, body, query } = request;
+
+  const prompt: string = method === "GET" ? query.prompt : body.prompt;
+  console.log(method, prompt);
+
+  const client = new OpenAIClient(
+    "https://chachachaudary.openai.azure.com/",
+    new AzureKeyCredential(process.env.AZOPENAI_API_KEY!),
+  );
+  const { id, created, choices, usage } = await client.getChatCompletions(
+    "chacha",
+    [
+      {
+        role: "system",
+        content: "You are a helpful assistant with name chacha.",
+      },
+      { role: "user", content: prompt },
+    ],
+  );
+
+  console.log(id, created, choices, usage);
+  response.status(200).json({
+    messageT: choices[0].message?.content,
+    data: {
+      id: id,
+      created: created,
+      choices: choices,
+      usage: usage,
+    },
+  });
+};
+
+export default handler;
