35249430e0aa58636b4ec39739c9623fe683d3ae
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-14 21:07:42 +0530

    del: unnecessary files

--- src/app/components/initiative.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default function Navbar() {
-  return (
-    <div className="w-[849px] h-[287px] text-center text-black text-[32px] font-semibold font-['Inter']">An Initiative by</div>
-  );
-}
