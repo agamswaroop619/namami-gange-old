8eced8990c5543ff7d595cc80776269b9dc26a84
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-14 23:03:55 +0530

    fix: components restructing

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -24,30 +24,40 @@ export default function Home() {
   );
 
   const handleReceiveMessage = (message: string) => {
-    // Update the array of received messages
     setReceivedMessages((prevMessages) => [...prevMessages, message]);
   };
 
-  const updateMessages = () => {
-    // Fetch the response for the updated question and update messages
-    getChat(currentQuestion).then((response) => {
+  const updateMessages = (question: string) => {
+    getChat(question).then((response) => {
       handleReceiveMessage(response);
     });
   };
 
   useEffect(() => {
-    updateMessages(); // Fetch initial message when the component mounts
-  }, []); // Empty dependency array to run only once when the component mounts
+    console.log("Component mounted");
+
+    // Fetch the initial response only on the client side
+    if (typeof window !== "undefined") {
+      getChat(currentQuestion).then((response) => {
+        handleReceiveMessage(response);
+      });
+    }
+  }, [currentQuestion]);
+
+  useEffect(() => {
+    console.log("currentQuestion changed:", currentQuestion);
+
+    // Fetch the response for the updated question and update messages
+    if (currentQuestion !== "Give a small introduction in of yours") {
+      updateMessages(currentQuestion);
+    }
+  }, [currentQuestion]);
 
   return (
     <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE]">
       <Navbar />
       <Messages messages={receivedMessages} />
-      <Textinput
-        onReceiveMessage={handleReceiveMessage}
-        setQuestion={setCurrentQuestion} // Pass a function to set the current question
-        updateMessages={updateMessages} // Pass a function to update messages
-      />
+      <Textinput setQuestion={setCurrentQuestion} />
     </main>
   );
 }
--- src/app/components/textinput.tsx
+++ src/app/components/textinput.tsx
@@ -3,36 +3,17 @@
 import React, { useState } from "react";
 
 interface TextinputProps {
-  onReceiveMessage: (message: string) => void;
-  setQuestion: React.Dispatch<React.SetStateAction<string>>;
-  updateMessages: () => void; // Callback to trigger message update in the parent component
+  setQuestion: (question: string) => void; // Update this line
 }
 
-const Textinput: React.FC<TextinputProps> = ({
-  onReceiveMessage,
-  setQuestion,
-  updateMessages,
-}) => {
+const Textinput: React.FC<TextinputProps> = ({ setQuestion }) => {
   const [question, setQuestionLocal] = useState<string>("");
 
-  const askQuestion = () => {
-    fetch("/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ prompt: question }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setQuestionLocal(""); // Clear the input field after asking the question
-        onReceiveMessage(data.messageT); // Pass the received message to the parent component
-        setQuestion(question); // Update the current question in the parent component
-        updateMessages(); // Trigger message update in the parent component
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+  const handleAskQuestion = () => {
+    // Pass the question to the parent component
+    setQuestion(question);
+    // Clear the input field after asking the question
+    setQuestionLocal("");
   };
 
   return (
@@ -44,7 +25,12 @@ const Textinput: React.FC<TextinputProps> = ({
         placeholder="Chachaji se poochey"
         className="input input-bordered w-full max-w-xs"
       />
-      <button onClick={askQuestion}>Ask</button>
+      <button
+        onClick={handleAskQuestion}
+        className="input input-bordered max-w-xs"
+      >
+        Ask
+      </button>
     </div>
   );
 };
