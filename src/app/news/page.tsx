da95a85f1b40da77d0de8f22890c10c93c7791b9
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-14 23:57:55 +0530

    add: news frontend (console log)

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,19 +1,37 @@
 import Image from "next/image";
 import Navbar from "../components/navbar";
-function getNews() {
-  return fetch("/api/news", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((json) => json.messageT);
+
+async function getNews() {
+  try {
+    const response = await fetch("/api/news", {
+      method: "GET", // Use GET method as per your server-side API configuration
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("All Results:", data.results); // Log all results
+
+    return data.messageT; // Assuming messageT is a property in your actual response
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    return null; // Handle error as needed
+  }
 }
+
 export default function Home() {
+  // Call the getNews function and log the result
+  getNews().then((result) => console.log("Result from getNews:", result));
+
   return (
     <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE]">
       <Navbar />
+      {/* Render your other components/content here */}
     </main>
   );
 }
