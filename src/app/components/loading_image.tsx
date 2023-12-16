d92ae49435cd2e0a4c9a25abe537662350a3ee44
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-17 00:16:50 +0530

    add: vercel update(2/2)

--- src/app/components/loading_image.tsx
+++ src/app/components/loading_image.tsx
@@ -1,10 +1,11 @@
 export default function Chachajiloading() {
   return (
-    <span className="loading loading-spinner scale-[5]">
+    <div>
       <img
-        alt="heelo"
+        alt="Loading..."
         src="https://media.licdn.com/dms/image/C4D0BAQEkWJOoS8iGLA/company-logo_200_200/0/1630516424447?e=2147483647&v=beta&t=KrmED5uJgzXgITjPsFnID5PE7oPNMy5k1BMUwqIlasc"
       />
-    </span>
+      <span className="loading loading-spinner"></span>
+    </div>
   );
 }
