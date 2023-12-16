8a61a909c29d5939cbbf61c1bafc36b12ea204f1
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-16 09:47:59 +0530

    add: loading template

---
+++ src/app/chat/loading.tsx
@@ -0,0 +1,8 @@
+import Chachajiloading from "../components/loading_image";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between min-w-full bg-[#B6D3FE] relative">
+      <Chachajiloading />
+    </main>
+  );
+}
---
+++ src/app/components/loading_image.tsx
@@ -0,0 +1,10 @@
+export default function Chachajiloading() {
+  return (
+    <span className="loading loading-spinner scale-[5]">
+      <img
+        alt="heelo"
+        src="https://media.licdn.com/dms/image/C4D0BAQEkWJOoS8iGLA/company-logo_200_200/0/1630516424447?e=2147483647&v=beta&t=KrmED5uJgzXgITjPsFnID5PE7oPNMy5k1BMUwqIlasc"
+      />
+    </span>
+  );
+}
