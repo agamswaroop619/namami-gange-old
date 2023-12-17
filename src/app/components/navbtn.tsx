55f5dfb95045d5e9fbe5ee910eebd80ab58651b2
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-17 13:29:59 +0530

    add: next button

---
+++ src/app/components/navbtn.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import next from "../components/icon-next.jpg";
+
+export default function Chachajifull() {
+  return <Image src={next} alt="" className="mix-blend-multiply scale-[0.1]" />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
 import Navbar from "./components/navbar";
 import Ask from "./components/ask_anything_chachaji";
 import Chachajifull from "./components/chachaji_image";
+import Navbtn from "./components/navbtn";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between min-w-full bg-[#B6D3FE] relative">
+    <main className="flex min-h-screen flex-col items-center justify-between min-w-full bg-[#B6D3FE] absolute">
       <Navbar />
       <Ask />
       <Chachajifull />
